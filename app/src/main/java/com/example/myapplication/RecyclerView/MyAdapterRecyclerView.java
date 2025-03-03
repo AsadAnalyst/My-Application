f72f0bb661effc2b9fc76c5eba7a32c4783084b3
AsadAnalyst <syed0055asad0@gmail.com>
2025-03-03 10:28:04 +0500

    Add Mobile Adapter Class and other

---
+++ app/src/main/java/com/example/myapplication/RecyclerView/MyAdapterRecyclerView.java
@@ -0,0 +1,58 @@
+package com.example.myapplication.RecyclerView;
+
+import android.icu.text.Transliterator;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.myapplication.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyAdapterRecyclerView extends RecyclerView.Adapter<MyAdapterRecyclerView.ViewHolder> {
+    private List<Mobile> mobileList;
+
+    public MyAdapterRecyclerView(List<Mobile> mobileList) {
+        this.mobileList = mobileList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.my_mobile_layout,parent,false);
+        ViewHolder obj=new ViewHolder(view);
+        return obj;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Mobile mobile= mobileList.get(position);
+        holder.mobilePrice.setText(mobile.getMobilePrice());
+        holder.mobileCompany.setText(mobile.getMobileCompany());
+        holder.mobileName.setText(mobile.getMobileName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mobileList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView mobileName;
+        TextView mobileCompany;
+        TextView mobilePrice;
+        public ViewHolder(@NonNull View view) {
+            super(view);
+            mobileName=view.findViewById(R.id.txtMobileName);
+            mobileCompany=view.findViewById(R.id.txtMobileCompany);
+            mobileName=view.findViewById(R.id.txtMobilePrice);
+        }
+
+    }
+}
--- app/src/main/java/com/example/myapplication/RecyclerView/RecyclerViewExample1Acticity.java
+++ app/src/main/java/com/example/myapplication/RecyclerView/RecyclerViewExample1Acticity.java
@@ -8,12 +8,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.myapplication.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RecyclerViewExample1Acticity extends AppCompatActivity {
     RecyclerView recyclerView;
+    MyAdapterRecyclerView myAdapterRecyclerView;
+    List <Mobile> mobileList = new ArrayList<>();
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +28,25 @@ public class RecyclerViewExample1Acticity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_recycler_view_example1_acticity);
         recyclerView=findViewById(R.id.recyclerViewExample1);
+        myAdapterRecyclerView=new MyAdapterRecyclerView(mobileList);
+        RecyclerView.LayoutManager layoutManager=new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(myAdapterRecyclerView);
+        LoadData();
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void LoadData(){
+        Mobile mobile1=new Mobile("Note 1","Sumsung","5000");
+        mobileList.add(mobile1);
+        Mobile mobile2=new Mobile("Note 2","Sumsung","10000");
+        mobileList.add(mobile2);
+        Mobile mobile3 = new Mobile("Note 3","Sumsung","20000");
+        mobileList.add(mobile3);
+        myAdapterRecyclerView.notifyDataSetChanged();
     }
 }
