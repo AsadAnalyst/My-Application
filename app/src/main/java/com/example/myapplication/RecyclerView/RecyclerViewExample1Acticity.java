03ff70dd0e34929eba06f267be0557f46bc92a4e
AsadAnalyst <syed0055asad0@gmail.com>
2025-03-03 09:14:57 +0500

    Bind  Recycler View id in main

--- app/src/main/java/com/example/myapplication/RecyclerView/RecyclerViewExample1Acticity.java
+++ app/src/main/java/com/example/myapplication/RecyclerView/RecyclerViewExample1Acticity.java
@@ -7,16 +7,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.myapplication.R;
 
 public class RecyclerViewExample1Acticity extends AppCompatActivity {
-
+    RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_recycler_view_example1_acticity);
+        recyclerView=findViewById(R.id.recyclerViewExample1);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
